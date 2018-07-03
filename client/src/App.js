0219df6f7571e2f6da1471d99cd611c2afbfa021
Deneb Pulsipher <pulsipherd@gmail.com>
2018-07-03 17:21:43 -0600

    add lots of components: No<atch, FakeAuth, Navbar, Dashboard, Login...

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,17 @@ import React from 'react';
 import Route from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
-import NoMatch from './components/NoMatch'
+import NoMatch from './components/NoMatch';
+import Navbar from './components/Navbar';
 
 const App = () => (
   <div>
-    <Route exact path="/" component={Home} />
-    <Route exact path="/about" component={About} />
-    <Route component={NoMatch} />
+    <Navbar />
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route component={NoMatch} />
+    </Switch>
   </div>
 );
     
--- client/src/components/NoMatch.js
+++ client/src/components/NoMatch.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NoMatch = () => (
   <div>
-  <h3>I'm afraid you're looking for a page from another book!</h3>
+    <h3>I'm afraid you're looking for a page from another book!</h3>
     <p>Please contact our <Link to={mailto: pulsipherd@gmail.com}> head pharmasist</Link> and tell him what's missing!</p>
   </div>
-)
+);
 
 export default NoMatch;
