b825d49c12fd916233709fadd67893e091f2affb
Deneb Pulsipher <pulsipherd@gmail.com>
2018-07-03 17:29:46 -0600

    update Navbar and App

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,9 @@ import Home from './components/Home';
 import About from './components/About';
 import NoMatch from './components/NoMatch';
 import Navbar from './components/Navbar';
+import Login from './components/Login';
+import Dashboard from './components/Dashboard';
+
 
 const App = () => (
   <div>
@@ -11,6 +14,8 @@ const App = () => (
     <Switch>
       <Route exact path="/" component={Home} />
       <Route exact path="/about" component={About} />
+      <Route path="/login" component={Login} />
+      <Route path="/dashboard" component={Dashboard} />
       <Route component={NoMatch} />
     </Switch>
   </div>
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { isAuthenticated } from '../FakeAuth';
 
 const styles = {
   active: {
@@ -14,7 +15,13 @@ const NavBar = () => (
     <NavLink exact activeStyle={styles.active} to="/">Home</NavLink>
     {' '}
     <NavLink activeStyle={styles.active} to="/about">About</NavLink>
+    {' '}
     <NavLink activeStyle={styles.active} to="/AllBooks">All Books</NavLink>
+    {' '}
+    {isAuthenticated() ?
+      <NavLink activeStyle={styles.active} to="/dashboard"> Dashboard</NavLink>
+      <NavLink activeStyle={styles.active} to="/login"> Login</NavLink>
+    }
   </nav>
 )
 
