3c21ae838ae7df224a6a4fd554bc3262ceb05f44
Deneb Pulsipher <pulsipherd@gmail.com>
2018-07-23 17:12:00 -0600

    add smalls to changeform, start other files

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -54,7 +54,11 @@ class NavBar extends Component {
           <Link to='/changeform'>
             <Menu.Item
               name='New Book'
+<<<<<<< HEAD
               id='changeform'
+=======
+              id='New Book'
+>>>>>>> add smalls to changeform, start other files
               active={this.props.location.pathname === '/'}
             /> </Link>
           {this.rightNavItems()}
