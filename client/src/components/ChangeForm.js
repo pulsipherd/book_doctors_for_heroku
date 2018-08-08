0bbe27670bcd0a25f6ff718d47c6e08a540721ef
Deneb Pulsipher <pulsipherd@gmail.com>
2018-08-08 11:27:43 -0600

    add delete button: not working

--- client/src/components/ChangeForm.js
+++ client/src/components/ChangeForm.js
@@ -5,35 +5,56 @@ class ChangeForm extends React.Component {
   defaultValues = {
     title: "Main Title: Subtitle, too, if it has one",
     author: "Author's First and Last Name",
-    blurb: "You can copy it from Amazon, GoodReads, or anywhere else, if you'd like. Just make sure you give credit where it's due! ('... according to http://...", 
-  difficulty: "Children's, Young Adult, College?",
+    blurb:
+      "You can copy it from Amazon, GoodReads, or anywhere else, if you'd like. Just make sure you give credit where it's due! ('... according to http://...",
+    difficulty: "Children's, Young Adult, College?",
     keywords: "Where's it set? What are some of the topics?",
-    lessons: "What are some of the struggles people have that this could help with?",
-  }
-  state = { ...this.dafaultValues }
+    lessons:
+      "What are some of the struggles people have that this could help with?"
+  };
+  state = { ...this.dafaultValues };
 
   componentDidMount() {
-    if (this.props.id)
-      this.setState({...this.props})
-  }
-  
-  handleSubmit = (e) => {
-    e.preventDefault();
-    let book = { ...this.state }
-    this.props.submit(book)
-    this.setState({ ...this.defaultValues })
+    if (this.props.id) this.setState({ ...this.props });
   }
 
-  handleChange = (e) => {
-    let { target: { id, value } } = e;
-    this.setState({ [id]: value })
-  }
+  handleSubmit = e => {
+    e.preventDefault();
+    let book = { ...this.state };
+    this.props.submit(book);
+    this.setState({ ...this.defaultValues });
+    console.log("Submitted correctly");
+  };
+
+  handleChange = e => {
+    let {
+      target: { id, value }
+    } = e;
+    this.setState({ [id]: value });
+  };
+
+  
+    
 
   render() {
-    let { title, author, blurb, difficulty, keywords, lessons, book_image } = this.state;
+    let {
+      title,
+      author,
+      blurb,
+      difficulty,
+      keywords,
+      lessons,
+      book_image
+    } = this.state;
     return (
       <Container>
-        <p>What book do you think could help other people in some way? We're mostly interested in novels, as fiction can produce such a cathartic experience enabling readers to forget themselves as they learn the lesson of the novel. Life-changing and well written nonfiction books are welcome, too, though. </p>
+        <p>
+          What book do you think could help other people in some way? We're
+          mostly interested in novels, as fiction can produce such a cathartic
+          experience enabling readers to forget themselves as they learn the
+          lesson of the novel. Life-changing and well written nonfiction books
+          are welcome, too, though.{" "}
+        </p>
         <form class="ui form" onSubmit={this.handleSubmit}>
           <label>Title</label>
           <input
@@ -42,7 +63,8 @@ class ChangeForm extends React.Component {
             value={title}
             onChange={this.handleChange}
             required
-            /> <br />
+          />{" "}
+          <br />
           <label>Author</label>
           <input
             id="author"
@@ -50,8 +72,16 @@ class ChangeForm extends React.Component {
             value={author}
             onChange={this.handleChange}
             required
-          /><br />
-          <label>Blurb <small>(this can by copied and pasted from any number of sources--Amazon, Goodreads, LibraryThing, etc--just make sure to include your source at the end!)</small></label>
+          />
+          <br />
+          <label>
+            Blurb{" "}
+            <small>
+              (this can by copied and pasted from any number of sources--Amazon,
+              Goodreads, LibraryThing, etc--just make sure to include your
+              source at the end!)
+            </small>
+          </label>
           <Form.TextArea
             width="100%"
             rows="8"
@@ -61,14 +91,16 @@ class ChangeForm extends React.Component {
             value={blurb}
             onChange={this.handleChange}
             required
-          /><br />
+          />
+          <br />
           <label>Difficulty </label>
           <select
             id="difficulty"
             placeholder="difficulty"
             value={difficulty}
             onChange={this.handleChange}
-            required>
+            required
+          >
             <option>infant</option>
             <option>toddler</option>
             <option>child</option>
@@ -76,14 +108,23 @@ class ChangeForm extends React.Component {
             <option>adult</option>
           </select>
           <br />
-          <label>Keywords <small>(these should be the actual topic of the book. If it's a novel, maybe the setting, or some of the activities that happen. Just anything that someone might want to look up for a friend who's interested in that sort of thing)</small></label>
+          <label>
+            Keywords{" "}
+            <small>
+              (these should be the actual topic of the book. If it's a novel,
+              maybe the setting, or some of the activities that happen. Just
+              anything that someone might want to look up for a friend who's
+              interested in that sort of thing)
+            </small>
+          </label>
           <input
             id="keywords"
             placeholder="keywords"
             value={keywords}
             onChange={this.handleChange}
             required
-          /><br />
+          />
+          <br />
           <label>Symptoms</label>
           <input
             id="lessons"
@@ -91,20 +132,46 @@ class ChangeForm extends React.Component {
             value={lessons}
             onChange={this.handleChange}
             required
-          /><br />
-          <label>URL of cover image <small> Soon we'll be pulling our cover images from the wonderful LibraryThing website, but until we get that up and running, go ahead and find an image of the cover somewhere online and paste the URL of the image here. Make sure to double check to make sure only the image itself pops up, by pasting the url into your browser first. It should take you to a page with only the image of the cover on it. Also, you should spend some time poking around the site where you find the image so that they get some benefit from our pulling the image from their servers.</small></label>
+          />
+          <br />
+          <label>
+            URL of cover image{" "}
+            <small>
+              {" "}
+              Soon we'll be pulling our cover images from the wonderful
+              LibraryThing website, but until we get that up and running, go
+              ahead and find an image of the cover somewhere online and paste
+              the URL of the image here. Make sure to double check to make sure
+              only the image itself pops up, by pasting the url into your
+              browser first. It should take you to a page with only the image of
+              the cover on it. Also, you should spend some time poking around
+              the site where you find the image so that they get some benefit
+              from our pulling the image from their servers.
+            </small>
+          </label>
           <input
             id="book_image"
             value={book_image}
             onChange={this.handleChange}
           />
           <br />
-          <Button color="yellow" onClick={this.submit}>Save My Book!</Button>
+          <Button color="yellow" onClick={this.submit}>
+            Save My Book!
+          </Button>
           <br />
         </form>
-        {" "}
+        <Button
+          color="red"
+          onClick={() => {
+            if (window.confirm("Are you sure you want to delete your book?")) {
+              this.deleteBook();
+            }
+          }}
+        >
+          Delete my book.
+        </Button>{" "}
       </Container>
-    )
+    );
   }
 }
 
--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -4,55 +4,66 @@ import { Link } from 'react-router-dom';
 import ChangeForm from './ChangeForm';
 
 class Dashboard extends React.Component {
-  state = { books: [], showForm: false }
-  
+  state = { books: [], showForm: false };
+
   componentDidMount() {
-    axios.get('/api/books')
-      .then(res => this.setState({ books: res.data }))
+    axios.get("/api/books").then(res => this.setState({ books: res.data }));
   }
 
   show() {
     let { books } = this.state;
     return (
       <ul>
-        {books.map(b =>
+        {books.map(b => (
           <li key={b.id}>
-            <Link to={`/books/${b.id}`} > {b.name}</Link>
+            <Link to={`/books/${b.id}`}> {b.name}</Link>
           </li>
-        )}
+        ))}
       </ul>
-    )
+    );
   }
-  
-  
+
   form() {
-    return <ChangeForm submit={this.submit} />
+    return <ChangeForm submit={this.submit} />;
   }
-  submit = (book) => {
-    let { books } = this.state
-    axios.post('/api/books', { book })
-      .then(res => this.setState({
-        books: [res.data, ...books], showForm:
-          false
-      }))
-  }
+  
+  submit = book => {
+    let { books } = this.state;
+    axios.post("/api/books", { book }).then(res =>
+      this.setState({
+        books: [res.data, ...books],
+        showForm: false
+      })
+    );
+  };
+
+  deleteBook = book => {
+    let { books } = this.state;
+    axios.delete("/api/books", { book }).then(res =>
+      this.setState({
+        books: [res.data, ...books]
+      })
+    );
+  };
 
   toggleForm = () => {
     this.setState(state => {
-      return { showForm: !state.showForm }
-    })
-  }
+      return { showForm: !state.showForm };
+    });
+  };
 
-    render() {
-      let { showForm } = this.state;
-      return (
-        <div>
-          <h2>Books</h2>
-          <button onClick={this.toggleForm}>{showForm ? 'Hide' : 'Show'} form</button>
-          { showForm ? this.form() : this.show() }
-        </div>
-      )
-    };
+  render() {
+    let { showForm } = this.state;
+    return (
+      <div>
+        <h2>Books</h2>
+        <button onClick={this.toggleForm}>
+          {showForm ? "Hide" : "Show"} form
+        </button>
+        {showForm ? this.form() : this.show()}
+      </div>
+    );
+  }
 }
 
 export default Dashboard;
