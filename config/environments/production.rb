92c0f9ca6d6bfeb58303eadbbc6b453f391f5199
Deneb Pulsipher <pulsipherd@gmail.com>
2018-08-16 23:19:44 -0600

    comment out added devise mailer stuff

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,22 +60,22 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
   # devise mailer setup
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-  config.action_mailer.delivery_method = :smtp
-  # change to true to allow email to be sent during development
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default :charset => "utf-8"
+  # config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  # config.action_mailer.delivery_method = :smtp
+  # # change to true to allow email to be sent during development
+  # config.action_mailer.perform_deliveries = true
+  # config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.default :charset => "utf-8"
 
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: "mail.google.com",####important
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV["bookdoctorsonline"],
-    password: ENV["B00kD0ct0rs4Devise"]
-  }
+  # config.action_mailer.smtp_settings = {
+  #   address: "smtp.gmail.com",
+  #   port: 587,
+  #   domain: "mail.google.com",####important
+  #   authentication: "plain",
+  #   enable_starttls_auto: true,
+  #   user_name: ENV["bookdoctorsonline"],
+  #   password: ENV["B00kD0ct0rs4Devise"]
+  # }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
